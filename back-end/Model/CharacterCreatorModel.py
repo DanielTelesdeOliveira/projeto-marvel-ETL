4de3de06e8dc83480402f2948f35c30a89bb2712
Daniel Teles de Oliveira <daniel.wteles@gmail.com>
2026-07-13 14:18:28 -0300

    refactor: rename model file

--- back-end/Model/CharacterCreatorModel.py
+++ back-end/Model/CharacterCreatorModel.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from database import Base
 
-class Character_Person(Base):
-    __tablename__ = "character_person"
+class Character_Creator(Base):
+    __tablename__ = "character_creator"
     id = Column(Integer, primary_key=True, index=True)
     character_id = Column(Integer, ForeignKey("characters.id", ondelete="CASCADE"))
-    person_id = Column(Integer, ForeignKey("person.id", ondelete="CASCADE"))
+    creator_id = Column(Integer, ForeignKey("person.id", ondelete="CASCADE"))
     
