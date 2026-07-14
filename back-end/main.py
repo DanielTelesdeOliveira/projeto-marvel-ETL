7729352ccb3071810dc06bc15405f4aa7895729a
Daniel Teles de Oliveira <daniel.wteles@gmail.com>
2026-07-13 22:51:42 -0300

    feat: enable CORS support for frontend

--- back-end/main.py
+++ back-end/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import database
 import Model
 from Controller import CharacterController, PersonController
 
 database.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://127.0.0.1:8000", "http://127.0.0.1:5500"],
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+                   )
+
 app.include_router(CharacterController.router)
 app.include_router(PersonController.router)
 
