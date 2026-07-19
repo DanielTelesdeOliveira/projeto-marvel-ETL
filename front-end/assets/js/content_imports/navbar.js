06cf7d41599280e4e12761d193131704d2a7bcd4
Daniel Teles de Oliveira <daniel.wteles@gmail.com>
2026-07-19 15:51:45 -0300

    feat: make navbar component self-contained

--- front-end/assets/js/content_imports/navbar.js
+++ front-end/assets/js/content_imports/navbar.js
@@ -1,7 +1,18 @@
-fetch('components/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    })
-    .catch(error => console.log("Error in import: ", error));
+async function loadNavBar(){
+    if(!document.querySelector(`link[href="/front-end/assets/css/navbar.css"]`)){
+        link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "/front-end/assets/css/navbar.css";
+        document.head.appendChild(link);
+    }
+
+    const response = await fetch('/front-end/components/navbar.html');
+    const html = await response.text();
+
+    document.getElementById("navbar").innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadNavBar().catch(error => console.log(error));
+});
 
