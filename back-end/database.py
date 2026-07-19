f82d40983f089cb4566f4c17e243134f04ddb64d
Daniel Teles de Oliveira <daniel.wteles@gmail.com>
2026-07-19 16:21:09 -0300

    chore: move database URL to .env

--- back-end/database.py
+++ back-end/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-#Cria arquivo local para desenvolvimento
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
