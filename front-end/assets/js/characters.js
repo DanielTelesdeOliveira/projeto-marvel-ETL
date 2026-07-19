467b58a2476b47216c6075a626651973d242c659
Daniel Teles de Oliveira <daniel.wteles@gmail.com>
2026-07-19 16:28:03 -0300

    refactor: update file path

--- front-end/assets/js/characters.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch("http://127.0.0.1:8000/characters/");
-    const data = await response.json();
-    document.getElementById("characters_message").textContent = data.message;
-});    
